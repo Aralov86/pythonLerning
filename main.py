3f59dac06851720dbf3de589bb73153e3a5726ec
Aralov86 <>
2021-06-02 11:04:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+# # Это комментарий
+# print("Hello world!")
+# print("Hello Netology!")
+
+# print(2 * 2)
+
+# greeting = "Hello world!"
+
+# print (greeting)
+
+# # CMD (ctrl) + /
+
+# x = 4
+# y = 16
+# z = x + y
+# print(z)
+# print(y ** x)
+
+
+# Данные
+# Простые
+# Числа
+# print(123 + 456)
+# print(123 - 456)
+# print(123 * 456)
+# print(123 / 456)
+# print(123 // 456)
+# print(123 % 456)
+# print(123 ** 456)
+# print(1.1 * 1.1)
+# # Строки
+# str1 = "Я строка с двойными кавычками"
+# str2 = "А я с одинарными"
+# str3 = '''А я вообще с тройными,
+# ещё и переносить строки могу'''
+# # print(str1)
+# # print(str2)
+# # print(str3)
+# print('"Хочу так пишу c кавычками"')
+# print("'А хочу так пишу с кавычками'")
+# # Конкатинирование строк
+# big_str = str1 + str2 + str3
+# print(big_str) 
+# x = str(1)
+# print (x)
+# print(len(str1))
+
+# x = None (пусто)
+
+#Пользовательский ввод
+# user_input = input("Введите своё имя: ") # <-
+# print("Привет,", user_input, ". Как твои дела?")
+
+# user_input = input('Введите задачу: ')
+# print("Вы ввели ", user_input)
+# print("Одна строка\nВторая строка")
+
+
+# Составные
+# Списки
+l = [1, 2, 3]
+print(int_list)
+# str_list = ["Hi", "Netology"]
+# print(str_list)
+# different_list = [1, 'text', 0.1, True, None]
+# Словари
+
+int_list.append(4)
+print(int_list)
+print
